a2d758357e1dc211df16f015c52da787d75ef2cd
Patrick Dahms <pat.dahms@googlemail.com>
2021-03-15 15:08:16 +0100

    fix nullable window

--- src/utils/convertLocalStorageIntegerToBoolean.ts
+++ src/utils/convertLocalStorageIntegerToBoolean.ts
@@ -1,13 +1,15 @@
 const convertLocalStorageIntegerToBoolean = (key: string): boolean => {
   // 📌 Info:
-  // `window` or `localStorage` are null via SSR
-  const localStorageItem = window?.localStorage?.getItem(key)
-  if (localStorageItem !== null) {
-    const probablyInteger = parseInt(localStorageItem, 10)
-    if (isNaN(probablyInteger)) {
-      return false
-    } else {
-      return Boolean(probablyInteger)
+  // `window` is undefined via SSR
+  if (typeof window !== 'undefined') {
+    const localStorageItem = window.localStorage.getItem(key)
+    if (localStorageItem !== null) {
+      const probablyInteger = parseInt(localStorageItem, 10)
+      if (isNaN(probablyInteger)) {
+        return false
+      } else {
+        return Boolean(probablyInteger)
+      }
     }
   }
   return false
